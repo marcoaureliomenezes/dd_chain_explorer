372a052bd48eb39e3161bd9ec350510f9bdb08a4
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2024-03-23 16:03:25 -0300

    Deleting old batch layer and segregating batch from fast layer

--- docker/infra/scylladb/file.sql
+++
@@ -1,8 +0,0 @@
-CREATE KEYSPACE IF NOT EXISTS operations WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
-
-
-CREATE TABLE IF NOT EXISTS api_keys_node_providers (
-  start TEXT, 
-  end TEXT, 
-  name TEXT PRIMARY KEY, 
-  num_req_1D INT);
