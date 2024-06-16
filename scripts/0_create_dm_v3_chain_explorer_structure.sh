41437af03177c6a268dcfbfcce30a066894280b6
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2024-06-15 21:55:50 -0300

    Creating dm_structure

--- scripts/0_create_dm_v3_chain_explorer_structure.sh
+++ scripts/0_create_dm_v3_chain_explorer_structure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-mkdir -p ./chain_explorer/layer_app
+mkdir -p ./docker/app_layer
 
-cd ./chain_explorer/app && \
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-stream-txs.git &&
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-watchers.git &&
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-watchers.git
+cd ./docker/app_layer && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-stream-txs.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-watchers.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-actors.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-monitor.git
