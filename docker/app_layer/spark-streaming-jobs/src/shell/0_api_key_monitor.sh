c769d9cfbc915cd5c53bfccd748020cb3143ddea
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2024-06-17 00:48:28 -0300

    Updating new streaming job

--- docker/app_layer/spark-streaming-jobs/src/shell/0_api_key_monitor.sh
+++ docker/app_layer/spark-streaming-jobs/src/shell/0_api_key_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SPARK_APPLICATION_PYTHON_LOCATION="/app/python/api_key_monitor.py"
+SPARK_APPLICATION_PYTHON_LOCATION="/app/python/0_api_key_monitor.py"
 
 echo "spark-submit                                              "
 echo " --master ${SPARK_MASTER_URL}                             "
@@ -14,5 +14,6 @@ spark-submit \
 --master ${SPARK_MASTER_URL} \
 --deploy-mode client \
 --total-executor-cores 1 \
+--executor-memory 512M \
 --packages ${KAFKA_CONNECTOR},${SCYLLA_CONNECTOR} \
 ${SPARK_APPLICATION_PYTHON_LOCATION}
