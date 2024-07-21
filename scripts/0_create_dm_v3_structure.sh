f3a01a9a525948c4c1f2f3d135b77b94b3a549c3
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2024-07-21 11:48:13 -0300

    Recommitin shell that build the dm_structure with applications.

---
+++ scripts/0_create_dm_v3_structure.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+mkdir -p ./docker/app_layer
+
+cd ./docker/app_layer && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-stream-txs.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-watchers.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-actors.git && \
+      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-monitor.git
