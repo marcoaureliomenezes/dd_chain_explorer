b4c6c462a6a0782ee7e6725fdd7b55973f037003
dadaia <marcoaurelioreislima@gmail.com>
2024-10-05 13:00:19 -0300

    continuous restructuring the project to improve it

---
+++ docker/app_layer/spark-streaming-jobs/src/2_job_bronze_fast_multiplex/spark_entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PYSPARK_FILEPATH="/app/2_job_bronze_fast_multiplex/app.py"
+PYFILES_SPARK_PATH="/app/utils/spark_utils.py"
+
+TOTAL_EXEC_CORES=1
+EXEC_MEMORY=512M
+
+
+echo "spark-submit                                                 "
+echo "    --deploy-mode client                                     "
+echo "    --executor-memory ${EXEC_MEMORY}                         "
+echo "    --total-executor-cores ${TOTAL_EXEC_CORES}               "
+echo "    --py-files ${PYFILES_PATH}                               "
+echo "    ${PYFILES_SPARK_PATH},${PYFILES_SCHEMA_REGISTRY_PATH}    "
+echo "    ${PYSPARK_FILEPATH}                                      "
+
+spark-submit                             \
+--deploy-mode client                     \
+--total-executor-cores $TOTAL_EXEC_CORES \
+--executor-memory $EXEC_MEMORY           \
+--py-files ${PYFILES_SPARK_PATH}         \
+${PYSPARK_FILEPATH}
--- scripts/0_create_dm_v3_structure.sh
+++ scripts/0_create_dm_v3_structure.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-mkdir -p ./docker/app_layer
-
-cd ./docker/app_layer && \
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-stream-txs.git && \
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-watchers.git && \
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-actors.git && \
-      git clone git@github.com:Dadaia-s-Chain-Analyser/onchain-monitor.git
-
-
-
 docker network create -d overlay --attachable layer_batch_prod
 docker network create -d overlay --attachable layer_fast_prod
 docker network create -d overlay --attachable layer_ops_prod
