1e4b7b5584dacbc7ebcf2e370cf4401ab11a7954
dadaia <marcoaurelioreislima@gmail.com>
2024-10-09 22:08:20 -0300

    Addinh onchain-stream-txs

--- docker/app_layer/spark-streaming-jobs/src/3_job_silver_app_logs/entrypoint.sh
+++ docker/app_layer/spark-streaming-jobs/src/3_job_silver_app_logs/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-PYSPARK_FILEPATH="/app/2_raw_to_bronze_logs_fast/app.py"
+PYSPARK_FILEPATH="/app/3_job_silver_app_logs/app.py"
 PYFILES_SPARK_PATH="/app/utils/spark_utils.py"
 PYFILES_SCHEMA_REGISTRY_PATH="/app/utils/schema_registry_utils.py"
 
