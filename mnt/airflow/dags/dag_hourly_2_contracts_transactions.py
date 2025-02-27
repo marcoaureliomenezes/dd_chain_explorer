e5145a23d838ebcc7b7480cb7bb89b96127505d0
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2025-02-27 01:27:55 -0300

    Starting to handle cache problem in capture and ingestion from batch layer

--- mnt/airflow/dags/dag_hourly_2_contracts_transactions.py
+++ mnt/airflow/dags/dag_hourly_2_contracts_transactions.py
@@ -2,8 +2,10 @@ import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import BranchPythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 
+from python_scripts.validate_cache_popular_contracts import PopularContractsCacheValidator
 LAKE_ENV_VARS = dict(
   SPARK_MASTER = os.getenv("SPARK_MASTER"),
   AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID"),
@@ -39,16 +41,23 @@ with DAG(
   catchup=True
   ) as dag:
 
-    starting_process = BashOperator(
-      task_id="starting_task",
+    STARTING_TASK = BashOperator(
+      task_id="STARTING_TASK",
       bash_command="""sleep 2"""
     )
 
-    get_popular_contracts_addresses = DockerOperator(
+
+    VALIDATE_CACHE = BranchPythonOperator(
+      task_id='VALIDATE_CACHE',
+      python_callable=PopularContractsCacheValidator().run,
+      op_args=["GET_POPULAR_CONTRACTS_AND_CACHE_IT", "BY_PASS"],
+      provide_context=True)
+
+    GET_POPULAR_CONTRACTS_AND_CACHE_THEM = DockerOperator(
       image="marcoaureliomenezes/spark-batch-jobs:1.0.0",
       **COMMON_DOCKER_OP,
       network_mode="vpc_dm",
-      task_id="get_popular_contracts_addresses",
+      task_id="GET_POPULAR_CONTRACTS_AND_CACHE_IT",
       entrypoint="sh /app/entrypoint.sh /app/periodic_spark_processing/1_get_popular_contracts.py",
       environment= {
         **LAKE_ENV_VARS,
@@ -60,12 +69,13 @@ with DAG(
       }
     )
 
+    BY_PASS_CACHE_FULLFILMENT = BashOperator(task_id="BY_PASS_CACHE_FULLFILMENT", bash_command='echo "BY_PASSING CACHE"' )
 
-    capture_and_ingest_popular_contracts_addresses_txs = DockerOperator(
+    CAPTURE_AND_STAGE_TXS_BY_CONTRACT = DockerOperator(
       image="marcoaureliomenezes/onchain-batch-txs:1.0.0",
       **COMMON_DOCKER_OP,
       network_mode="vpc_dm",
-      task_id="capture_and_ingest_popular_contracts_addresses_txs",
+      task_id="CAPTURE_AND_STAGE_TXS_BY_CONTRACT",
       entrypoint="python /app/batch_ingestion/1_capture_and_ingest_contracts_txs.py",
       environment= {
       "S3_URL": os.getenv("S3_URL"),
@@ -93,11 +103,11 @@ with DAG(
     )
 
 
-    bronze_popular_contracts_addresses_txs = DockerOperator(
+    INGEST_TX_DATA_TO_BRONZE = DockerOperator(
       image="marcoaureliomenezes/spark-batch-jobs:1.0.0",
       **COMMON_DOCKER_OP,
       network_mode="vpc_dm",
-      task_id="bronze_popular_contracts_addresses_txs",
+      task_id="INGEST_TX_DATA_TO_BRONZE",
       entrypoint="sh /app/entrypoint.sh /app/periodic_spark_processing/2_ingest_txs_data_to_bronze.py",
       environment= {
         **LAKE_ENV_VARS,
@@ -110,4 +120,4 @@ with DAG(
       }
     )
 
-    starting_process >> get_popular_contracts_addresses >> capture_and_ingest_popular_contracts_addresses_txs >> bronze_popular_contracts_addresses_txs
+    STARTING_TASK >> VALIDATE_CACHE >> [GET_POPULAR_CONTRACTS_AND_CACHE_THEM, BY_PASS_CACHE_FULLFILMENT] >> CAPTURE_AND_STAGE_TXS_BY_CONTRACT >> INGEST_TX_DATA_TO_BRONZE
---
+++ mnt/airflow/dags/python_scripts/validate_cache_popular_contracts.py
@@ -0,0 +1,12 @@
+
+
+
+class PopularContractsCacheValidator:
+
+    def __init__(self):
+        self.pop = None
+        self.cache = None
+
+    
+    def run(self, **kwargs):
+        return False
